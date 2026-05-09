8e7fda350b0e406ae7e7a68071c0ec5a7eca6604
Developer <nurmandityomoussa@gmail.com>
2026-05-10 00:43:57 +0700

    hapus vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,13 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+
   plugins: [
     react(),
     legacy()
   ],
+
   test: {
     globals: true,
     environment: 'jsdom',
